791141a804450b715a1fe957b2172b8a86f1b407
Timothy Yeo <evilsengokunadeko@gmail.com>
2020-08-01 12:51:57 +0800

    unzipped the file

---
+++ Heritage Game Jam/Heritage Game Jam/Assets/Scripts/References.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class References : MonoBehaviour
+{
+    public static References instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    //access scene objects here!
+    public GameObject testGameObject;
+
+
+}
